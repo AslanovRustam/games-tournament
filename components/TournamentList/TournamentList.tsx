21e77a490f2419bfb013b599a710c1544711dd7e
AslanovRustam <aslanovrg@gmail.com>
2025-11-07 01:43:54 +0200

    upd TournamentList cmp with edge case

--- components/TournamentList/TournamentList.tsx
+++ components/TournamentList/TournamentList.tsx
@@ -12,10 +12,12 @@ function TournamentList({ initialTournaments }: Props) {
   const [tournaments, setTournaments] = useState<ITournament[]>(
     initialTournaments || []
   );
+  const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleSearch = async (query: string) => {
     setLoading(true);
+    setSearchQuery(query);
     try {
       const { tournaments } = await fetchTournaments({ searchText: query });
       setTournaments(tournaments);
@@ -30,15 +32,22 @@ function TournamentList({ initialTournaments }: Props) {
   return (
     <section className="w-full px-[5vw] py-10 flex flex-col gap-8">
       <SearchBar onSearch={handleSearch} />
-      <ul className="grid w-full gap-6 grid-cols-[repeat(auto-fit,minmax(30%,1fr))]">
-        {loading ? (
-          <Skeleton />
-        ) : (
-          tournaments?.map((tournament) => (
+      {loading ? (
+        <Skeleton />
+      ) : tournaments.length > 0 ? (
+        <ul className="grid w-full gap-6 grid-cols-[repeat(auto-fit,minmax(30%,1fr))]">
+          {tournaments.map((tournament) => (
             <TournamentItem key={tournament.id} tournament={tournament} />
-          ))
-        )}
-      </ul>
+          ))}
+        </ul>
+      ) : (
+        searchQuery && (
+          <p className="text-center mt-10 text-lg">
+            No tournaments found for{" "}
+            <span className="font-semibold ">“{searchQuery}”</span>.
+          </p>
+        )
+      )}
     </section>
   );
 }
